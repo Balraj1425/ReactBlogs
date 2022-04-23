c5ee46aad69567a47ec72af070c8091c08916336
Balraj Singh Bhullar <bhullar.bs@gmail.com>
2022-04-23 13:35:01 +0530

    WIP: About us

--- blogapp/src/App.js
+++ blogapp/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
+import Aboutus from './components/aboutus/aboutus';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Aboutus></Aboutus>
       <Footer></Footer>
     </div>
   );
